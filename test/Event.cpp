5f8913500dfc7ff1586e2f062fcbdd1b70ce045f
Matt Fichman <matt.fichman@gmail.com>
2013-11-27 18:03:55 -0500

    Added new tests

---
+++ test/Event.cpp
@@ -0,0 +1,25 @@
+
+#include <coro/coro.hpp>
+
+int main() {
+    auto event = coro::Event();
+    auto trigger = false;
+
+    auto notifier = coro::start([&]() {
+        printf("notified\n");
+        trigger = true;
+        event.notifyAll();
+    });
+
+    auto waiter = coro::start([&]() {
+        printf("waiting\n");
+        event.wait([&]() { return trigger; }); 
+        printf("done\n");
+        exit(0);
+
+    });
+
+    coro::run();
+
+    return 0;
+}
---
+++ test/Stack.cpp
@@ -0,0 +1,28 @@
+
+
+#include <coro/coro.hpp>
+
+using namespace coro;
+
+void recurse(int n) {
+    char data[10000];
+    data[0] = n;
+    if (n == 0) { return; }
+    else { recurse(n-1); }
+}
+
+
+int main() {
+    // Check that coroutines do not use too much stack mem
+    std::vector<Ptr<Coroutine>> coros;
+
+    for (auto i = 0; i < 1000; ++i) {
+        for (auto i = 0; i < 1000; ++i) {
+            coros.push_back(coro::start(std::bind(recurse, 100)));
+        }
+        hub()->quiesce();
+        coros.clear();
+    }
+
+    return 0;
+}
